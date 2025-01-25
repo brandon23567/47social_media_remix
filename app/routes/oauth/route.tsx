8ef8ae81cee43cd42e153abef361e7fcaee49054
brandon23567 <brandonmahuvava0@gmail.com>
2025-01-25 23:31:29 +0530

    fixing some changes

--- app/routes/oauth/route.tsx
+++ app/routes/oauth/route.tsx
@@ -1,32 +1,44 @@
 import "./oauth_page.css";
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { useState, useEffect } from "react";
 
 interface LoaderData{
     access_token: string,
     state: string
 };
 
-export async function loader({request}: LoaderFunctionArgs){
-    const url = new URL(request.url);
-    const hash = url.hash.substring(1);
+// export async function loader({request}: LoaderFunctionArgs){
+//     const url = new URL(request.url);
+//     const hash = url.hash.substring(1);
 
-    const params = new URLSearchParams(hash);
+//     const params = new URLSearchParams(hash);
 
-    const accessToken = params.get("access_token");
-    const stateValue = params.get("state");
+//     const accessToken = params.get("access_token");
+//     const stateValue = params.get("state");
 
-    return{
-        "access_token": accessToken,
-        "state": stateValue
-    };
-}
+//     return{
+//         "access_token": accessToken,
+//         "state": stateValue
+//     };
+// }
 
 export default function OAuthPage(){
-    const loaderData = useLoaderData<LoaderData>();
+    // const loaderData = useLoaderData<LoaderData>();
+    const [accessToken, setAccessToken] = useState<string | null>("");
+    const [stateValue, setStateValue] = useState<string | null>("");
 
-    console.log("access token is: ", loaderData.access_token);
-    console.log("state value is: ", loaderData.state);
+    useEffect(() => {
+        const hash = window.location.hash.substring(1); // Remove the leading '#'
+        const params = new URLSearchParams(hash);
+
+        setAccessToken(params.get("access_token"));
+        setStateValue(params.get("state"));
+
+        console.log("Access Token:", params.get("access_token"));
+        console.log("State:", params.get("state"));
+
+    }, []);
 
     return(
         <div className="main_outer_oauth_page_container">
